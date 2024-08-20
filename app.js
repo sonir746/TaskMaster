01c63b1db958db4d87afd4b3c83d90bfc4221087
RAHUL SONI <sonir7469@gmail.com>
2024-08-20 09:06:18 +0530

    Update app.js
    changing local database to atlas

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+// mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb+srv://sonir746:usemongodb@cluster0.dc3bi.mongodb.net/todolistDB");
 
 const itemsSchema = {
   name: String,
