86c62149b4ef1cfe07004b5c068fdc71ee478561
rahul-soni-746 <rahulsoni7469@gmail.com>
2024-09-17 23:49:15 +0530

    Update api

--- api/app.js
+++ api/app.js
@@ -4,18 +4,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { default: mongoose } = require("mongoose");
 const { name } = require("ejs");
-const _ = require('lodash');
+const _ = require("lodash");
 const date = require(__dirname + "/date.js");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost:27017/todolistDB");
-mongoose.connect("mongodb+srv://sonir746:usemongodb@cluster0.dc3bi.mongodb.net/todolistDB");
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
 const itemsSchema = {
   name: String,
@@ -24,34 +23,27 @@ const itemsSchema = {
 const Item = mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
-  name: "Welcome to your Todolist!"
-})
+  name: "Welcome to your Todolist!",
+});
 const item2 = new Item({
-  name: "Hit the + button to add a new item."
-})
+  name: "Hit the + button to add a new item.",
+});
 const item3 = new Item({
-  name: "<-- Hit this to delete an item.>"
+  name: "<= Hit this to delete an item.",
 });
 
-
-
-
-
-const defaultItem = [item1, item2, item3]
+const defaultItem = [item1, item2, item3];
 
 const listSchema = new mongoose.Schema({
-  name: { type: String, unique: true },  // Add unique constraint
-  items: [itemsSchema]
+  name: { type: String, unique: true },
+  items: [itemsSchema],
 });
 
-
-const List = mongoose.model("List", listSchema)
+const List = mongoose.model("List", listSchema);
 
 const day = date.getDate();
 
-
-app.get("/about", function (req, res)
-{
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
@@ -59,109 +51,81 @@ app.get("/health", (req, res) => {
   res.send("OK");
 });
 
-
-
-
-
-app.get("/", function (req, res)
-{
+app.get("/", function (req, res) {
   Item.find()
-    .then(result =>
-    {
-
+    .then((result) => {
       if (result.length === 0) {
         Item.insertMany(defaultItem)
-          .then(result =>
-          {
-
-            res.redirect("/")
+          .then((result) => {
+            res.redirect("/");
           })
-          .catch(err =>
-          {
-            console.log(err)
-          })
-
-
+          .catch((err) => {
+            console.log(err);
+          });
       } else {
         res.render("list", { listTitle: day, newListItems: result });
       }
-
     })
-    .catch(err =>
-    {
-      console.log(err)
-    })
-
-
-
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-
-app.post("/", function (req, res)
-{
+app.post("/", function (req, res) {
   const itemName = req.body.newItem.trim();
   const listName = req.body.list;
 
   const item = new Item({
-    name: itemName
-  })
+    name: itemName,
+  });
 
   if (listName === day) {
     if (itemName.length !== 0) {
-      item.save()
-        .then(() =>
-        {
-          res.redirect("/")
+      item
+        .save()
+        .then(() => {
+          res.redirect("/");
         })
-        .catch(err =>
-        {
-          console.log(err)
+        .catch((err) => {
+          console.log(err);
         });
+    }else{
+      res.redirect("/")
     }
-
   } else {
     List.findOne({ name: listName })
-      .then(result =>
-      {
+      .then((result) => {
         if (itemName.length !== 0) {
           result.items.push(item);
-          result.save()
-            .then(() =>
-            {
-              res.redirect("/" + listName)
+          result
+            .save()
+            .then(() => {
+              res.redirect("/" + listName);
             })
-            .catch(err =>
-            {
-              console.log(err)
+            .catch((err) => {
+              console.log(err);
             });
+        }else{
+          res.redirect("/" + listName)
         }
-
       })
-      .catch(err =>
-      {
-        console.log(err)
-      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-
-
 });
 
-
-app.post("/delete", (req, res) =>
-{
-
+app.post("/delete", (req, res) => {
   const checkboxId = req.body.checkbox;
   const listName = req.body.listName;
 
   if (listName === day) {
     // Remove from the Item collection
     Item.findByIdAndDelete(checkboxId)
-      .then(() =>
-      {
+      .then(() => {
         res.redirect("/");
       })
-      .catch(err =>
-      {
+      .catch((err) => {
         console.log(err);
       });
   } else {
@@ -170,62 +134,47 @@ app.post("/delete", (req, res) =>
       { name: listName },
       { $pull: { items: { _id: checkboxId } } }
     )
-      .then(() =>
-      {
+      .then(() => {
         res.redirect("/" + listName);
       })
-      .catch(err =>
-      {
+      .catch((err) => {
         console.log(err);
       });
   }
 });
 
-
-
-
-app.get("/:customListName", function (req, res)
-{
+app.get("/:customListName", function (req, res) {
   let customListName = _.startCase(req.params.customListName);
 
   List.findOne({ name: customListName })
-    .then(result =>
-    {
+    .then((result) => {
       if (!result) {
         const list = new List({
           name: customListName,
-          items: defaultItem
+          items: defaultItem,
         });
 
-        list.save()
-          .then(() =>
-          {
+        list
+          .save()
+          .then(() => {
             res.redirect("/" + customListName);
           })
-          .catch(err =>
-          {
+          .catch((err) => {
             console.log("Error saving new list:", err);
           });
       } else {
-        res.render("list", { listTitle: customListName, newListItems: result.items });
+        res.render("list", {
+          listTitle: customListName,
+          newListItems: result.items,
+        });
       }
     })
-    .catch(err =>
-    {
+    .catch((err) => {
       console.log("Error finding list:", err);
     });
 });
 
-
-
-
-
-
-
-
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, function ()
-{
+app.listen(PORT, function () {
   console.log(`Server started on port ${PORT}`);
 });
